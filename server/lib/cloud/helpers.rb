120cb6c0ed78eec2ad8ba42862eb1c5b139f5d26
GitLab <gitlabdev@dianping.com>
2013-05-13 18:48:09 +0800

    bind ip domain

---
+++ server/lib/cloud/helpers.rb
@@ -0,0 +1,22 @@
+require 'net/http'
+
+module Cloud
+  module Helpers
+    def bind_ip(ip,domain)
+      uri = URI("http://#{Settings.diors.dns_service.ip}:#{Settings.diors.dns_service.port}/addname")
+      params = { :rdata => "#{ip}", :name => "#{domain}" }
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(uri)
+      res.code == "200" ? true : false
+    end  
+
+    def release_ip(ip,domain)
+      uri = URI("http://#{Settings.diors.dns_service.ip}:#{Settings.diors.dns_service.port}/delname")
+      params = { :rdata => "#{ip}", :name => "#{domain}" }
+      uri.query = URI.encode_www_form(params)
+      res = Net::HTTP.get_response(uri)
+      res.code == "200" ? true : false
+    end
+
+  end  
+end
