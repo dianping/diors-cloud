027addf2880fd6f8d975c30f4303c69d93b12ba2
GitLab <gitlabdev@dianping.com>
2013-05-13 18:53:32 +0800

    dns add notes

--- server/lib/cloud/helpers.rb
+++ server/lib/cloud/helpers.rb
@@ -2,6 +2,7 @@ require 'net/http'
 
 module Cloud
   module Helpers
+    # you may add the dns #{Settings.diors.dns_service.ip}
     def bind_ip(ip,domain)
       uri = URI("http://#{Settings.diors.dns_service.ip}:#{Settings.diors.dns_service.port}/addname")
       params = { :rdata => "#{ip}", :name => "#{domain}" }
