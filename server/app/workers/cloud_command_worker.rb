649d51729fbc76dd7b4e51f923c9a2900509472c
kenshin54 <himurakenshin54@gmail.com>
2013-05-15 12:15:20 +0800

    Disable retry when command execute fail.

--- server/app/workers/cloud_command_worker.rb
+++ server/app/workers/cloud_command_worker.rb
@@ -1,7 +1,7 @@
 class CloudCommandWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :diors_command
+  sidekiq_options queue: :diors_command, retry: false
 
   def perform(action, *args)
     Cloud::Command.const_get(action.to_s.classify).new(*args).execute
